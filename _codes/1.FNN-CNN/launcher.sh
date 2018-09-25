764c8d09205026ad21c45341b477d98c91f3f24d
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2018-09-25 11:30:34 +0200

    Fixed code and launcher

--- _codes/1.FNN-CNN/launcher.sh
+++ _codes/1.FNN-CNN/launcher.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # @ job_name= code_lab1.1
 # @ class = training
-# @ initialdir= /gpfs/home/nctXX/
+# @ initialdir= /home/nct01/nct01YYY
 # @ output= %j.out
 # @ error= %j.err
 # @ total_tasks= 1
 # @ gpus_per_node= 4
 # @ cpus_per_task= 1
 # @ features= k80
-# @ wall_clock_limit= 00:00:03
+# @ wall_clock_limit= 00:01:00
 
-#SBATCH --reservation=PATC_DL2
-
-::Code for running in MT with Python 2.7.12
 module purge
 #module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
-#module load K80/default mkl/2017.0.098 cuda/7.5 CUDNN/5.1.3 intel-opencl/2016 python/2.7.12_ML
+module load K80/default mkl/2017.0.098 cuda/7.5 CUDNN/5.1.3 intel-opencl/2016 python/2.7.12_ML
 python /gpfs/projects/bsc28/MAI-DL/lab1_MNIST/code_lab1.1.py 
