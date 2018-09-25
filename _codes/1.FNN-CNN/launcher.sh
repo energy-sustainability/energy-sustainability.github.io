b68bdaf255540655adb513d2868a4d39256b6098
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2018-09-25 12:01:36 +0200

    Moved comment

--- _codes/1.FNN-CNN/launcher.sh
+++ _codes/1.FNN-CNN/launcher.sh
@@ -11,6 +11,6 @@
 # @ wall_clock_limit= 00:01:00
 
 module purge
-#module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
-module load K80/default mkl/2017.0.098 cuda/7.5 CUDNN/5.1.3 intel-opencl/2016 python/2.7.12_ML
+module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
+#module load K80/default mkl/2017.0.098 cuda/7.5 CUDNN/5.1.3 intel-opencl/2016 python/2.7.12_ML
 python /gpfs/projects/bsc28/MAI-DL/lab1_MNIST/code_lab1.1.py 
