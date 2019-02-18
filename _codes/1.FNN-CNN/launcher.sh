05200adbdf9cb8eefca2e1175ecc3ef79d78464b
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2019-02-18 16:41:22 +0100

    New version spring 18-19

--- _codes/1.FNN-CNN/launcher.sh
+++ _codes/1.FNN-CNN/launcher.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-# @ job_name= code_lab1.1
-# @ class = training
-# @ initialdir= /home/nct01/nct01YYY
-# @ output= %j.out
-# @ error= %j.err
-# @ total_tasks= 1
-# @ gpus_per_node= 4
-# @ cpus_per_task= 1
-# @ features= k80
-# @ wall_clock_limit= 00:01:00
 
-module purge
-module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
-#module load K80/default mkl/2017.0.098 cuda/7.5 CUDNN/5.1.3 intel-opencl/2016 python/2.7.12_ML
-python /gpfs/projects/bsc28/MAI-DL/lab1_MNIST/code_lab1.1.py 
+#SBATCH --job-name="test_mnist"
+
+#SBATCH --workdir=.
+
+#SBATCH --output=mnist_%j.out
+
+#SBATCH --error=mnist_%j.err
+
+#SBATCH --ntasks=1
+
+#SBATCH --gres gpu:1
+
+#SBATCH --time=00:02:00
+
+module purge; module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
+
+python code.py
