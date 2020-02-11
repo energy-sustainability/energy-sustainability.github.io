6be312755bd0290b62a1316fde7bf0e828b1560c
Dario Garcia <dario.garcia@bsc.es>
2020-02-11 18:08:55 +0100

    Updated launcher to P9

--- _codes/1.FNN-CNN/launcher.sh
+++ _codes/1.FNN-CNN/launcher.sh
@@ -10,12 +10,12 @@
 
 #SBATCH --error=test_mnist_%j.err
 
-#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=40
 
 #SBATCH --gres gpu:1
 
 #SBATCH --time=00:02:00
 
-module purge; module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
+module purge; module load ffmpeg/4.0.2 gcc/6.4.0 cuda/9.1 cudnn/7.1.3 openmpi/3.0.0 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.7 szip/2.1.1 opencv/3.4.1 python/3.6.5_ML
 
 python mnist_fnn_example.py
